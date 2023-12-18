14c0c1a7f8ab63a1e8b212e54e813dc959a0f6aa
iamben0 <benedict.ongjp@gmail.com>
2023-12-19 01:10:43 +0800

    chore: config baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: `https://${organizationName}.github.io`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/${projectName}/}`,
+  baseUrl: `/${projectName}/`,
   trailingSlash: false,
 
   // GitHub pages deployment config.
