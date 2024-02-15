b7d22a94418477d43c7034ab6806530ffe15d6db
raizel <gonzalesraizel@gmail.com>
2024-02-15 16:44:01 +0800

    fix: light/dark mode toggle button stays white in dark mode

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -47,7 +47,7 @@ const FeatureList: FeatureItem[] = [
 function Feature({title, Svg, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
+      <div className="text--center feature-item-component">
         <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
